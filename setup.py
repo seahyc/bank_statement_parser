5ffeade0be3468cae845b95c06f2c8a452c0b1f3
Ying Cong <yingcong@glints.com>
2024-10-10 14:05:24 +0800

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def get_version():
     version = os.environ.get('GITHUB_REF_NAME')
     if version and version.startswith('v'):
         return version[1:]  # Remove 'v' prefix
-    return '0.2.0'  # Default version if not a release
+    return '0.2.1'  # Default version if not a release
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
